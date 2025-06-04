6ac5abe0f4845c48118f0fa24ecf38080f84be2a
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2025-06-05 01:26:36 +0200

    Motor params updated

--- src/motor_config.h
+++ src/motor_config.h
@@ -2,8 +2,8 @@
 #define MOTOR_CONFIG_H
 
 #define MOTOR_POLE_PAIR_NUMBER  50
-#define MOTOR_PHASE_RESISTANCE  0.88f // [Ohms]
+#define MOTOR_PHASE_RESISTANCE  1.3f // [Ohms]
 #define MOTOR_KV_RATING         NOT_SET // [RPM/V] (RMS value)
-#define MOTOR_PHASE_INDUCTANCE  0.0034f // [H]
+#define MOTOR_PHASE_INDUCTANCE  0.0024f // [H]
 
 #endif // MOTOR_CONFIG_H
