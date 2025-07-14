bd8719f5233362e40f05ec962f801b76c5b93174
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2025-07-14 23:04:07 +0200

    Added CanOpenSTM32

---
+++ lib/CanOpenSTM32-wrapper/src/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <Arduino.h>
+#include "stm32g4xx_hal_fdcan.h"
+
+#endif // MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "bsp/cln17_v2.h"
 #include "SimpleFOC_setup.h"
 #include "config.h"
+#include "CO_app_STM32.h"
 
 HardwareTimer foc_timer = HardwareTimer(TIM8);
 
@@ -20,10 +21,21 @@ void setup() {
     foc_timer.setOverflow(FOC_LOOP_FREQUENCY, HERTZ_FORMAT);
     foc_timer.attachInterrupt(foc_timer_interrupt);
     foc_timer.resume();
+
+    // CANopenNodeSTM32 canOpenNodeSTM32;
+    // canOpenNodeSTM32.CANHandle = nullptr;
+    // canOpenNodeSTM32.HWInitFunction = nullptr;
+    // canOpenNodeSTM32.timerHandle = nullptr;
+    // canOpenNodeSTM32.desiredNodeID = 29;
+    // canOpenNodeSTM32.baudrate = 125;
+    // canopen_app_init(&canOpenNodeSTM32);
 }
 
 void loop() {
     // loopFOC();
     // current_sensor.getPhaseCurrents();
     // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_10);
+
+    // canopen_app_interrupt();
+    // canopen_app_process();
 }
